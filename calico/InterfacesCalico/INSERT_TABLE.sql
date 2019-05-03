236e2df729756b331a711acf761338df3da6cc05
Matias <matias_lavarello@hotmail.com>
2019-05-03 16:37:35 -0300

    #Insert_table_script_interface

---
+++ calico/InterfacesCalico/INSERT_TABLE.sql
@@ -0,0 +1,18 @@
+INSERT INTO [dbo].[BIANCHI_PROCESS]
+           ([inicio]
+           ,[fin]
+           ,[cant_lineas]
+           ,[estado]
+           ,[process_id]
+           ,[maquina]
+           ,[interfaz]
+           ,[fecha_ultima])
+     VALUES
+           (NULL
+           ,NULL
+           ,NULL
+           ,NULL
+           ,NULL
+           ,NULL
+           ,'Clientes'
+           ,NULL)
